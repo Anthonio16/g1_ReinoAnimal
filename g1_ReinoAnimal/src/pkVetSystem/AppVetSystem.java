78ef014423194c9deb5a8e518b622395c4f1379e
Ariana <thais.alomoto@gmail.com>
2025-11-12 11:21:40 -0500

    Integración de veterinario en AppVetSystem

--- g1_ReinoAnimal/src/pkVetSystem/AppVetSystem.java
+++ g1_ReinoAnimal/src/pkVetSystem/AppVetSystem.java
@@ -1,7 +1,11 @@
 package pkVetSystem;
 
+import pkVetSystem.pkhumano.Veterinario;
 import pkVetSystem.pkReinoViviente.pkAnimal.Camaleon;
 import pkVetSystem.pkReinoViviente.pkAnimal.Hiena;
+import pkVetSystem.pkReinoViviente.pkAnimal.PezPayaso;
+import pkVetSystem.pkReinoViviente.pkAnimal.AnimalVertebrado;
+
 
 public class AppVetSystem {
     
@@ -15,6 +19,7 @@ public class AppVetSystem {
         //Instanciar animales
         Camaleon camaleon = new Camaleon("Federico", 3, 300.5f, "Reptil", "Verde Oscuro");
         Hiena hiena = new Hiena("Alfonso", 7, 65.0f, "Mamifero");
+        PezPayaso pezPayaso = new PezPayaso("Nemo", 2, 0.2f, "Pez");
 
 
         //Interaccion del Camaleon
@@ -22,5 +27,33 @@ public class AppVetSystem {
         camaleon.cambiarColor("Celeste", hiena);
         camaleon.treparRama(hiena);
 
+        //Instanciar veterinario
+        Veterinario vet = new Veterinario("1234567890","Juan","Pérez");
+        vet.setClave("JuanVet","clave123");
+
+        vet.entrarCentroConservacion();
+        vet.registrarAnimal(camaleon);
+        vet.registrarAnimal(hiena);
+        vet.registrarAnimal(pezPayaso);
+        
+        atenderAnimalConReporte(vet, camaleon);
+        atenderAnimalConReporte(vet, hiena);
+        atenderAnimalConReporte(vet, pezPayaso);
+
+        //Mostrar total de animales atendidos
+        System.out.println("Total de animales atendidos por el veterinario " + vet.getNombre() + ": " + vet.getAnimalesAtendidos());
+    
+
+
+    }
+    private void atenderAnimalConReporte(Veterinario vet, AnimalVertebrado animal) {
+        System.out.println("\n");
+        vet.atenderAnimal(animal);
+        System.out.println(vet.observarSintomas(animal));
+        System.out.println(vet.diagnosticarAnimal(animal));
+        System.out.println(vet.darTratamiento(animal));
+        System.out.println("\n--- REPORTE FINAL ---");
+        System.out.println(vet.generarReporte(animal));
+        System.out.println("------------------------------------------------------");
     }
 }
