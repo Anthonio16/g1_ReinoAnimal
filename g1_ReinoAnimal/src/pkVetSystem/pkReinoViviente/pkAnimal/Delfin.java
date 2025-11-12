299eb5087ee9cf9ed38519a3040b123341383054
Wifelywax <josue.astudillo@epn.edu.ec>
2025-11-12 12:04:10 -0500

    Ampliacion del Use Case Diagram y eliminacion de delfin y clase acuatico :v

--- g1_ReinoAnimal/src/pkVetSystem/pkReinoViviente/pkAnimal/Delfin.java
+++
@@ -1,9 +0,0 @@
-package pkVetSystem.pkReinoViviente.pkAnimal;
-
-public class Delfin extends Acuatico {
-
-    public Delfin(String nombre, Integer edad, Float peso, String especie) {
-        super(nombre, edad, peso, especie);
-    }
-
-}
